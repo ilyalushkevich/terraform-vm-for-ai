d5da823358b0556f94977c950d73e9a68eeac2ba
Ilya <xpaveldurov@gmail.com>
2025-08-23 12:47:12 +0300

    fix(outputs): a output name

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "connect_to_runner_vm" {
-  description = "Подключение к ВМ раннера через SSH."
+output "connect_to_vm" {
+  description = "Подключение к ВМ через SSH."
   value = format("ssh %s@%s",
     "deepseek-practice", yandex_compute_instance.vm.network_interface[0].nat_ip_address
   )
